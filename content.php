bdc381bc2de0bc745a350e9a62514a722737b636
Thomas Stein <firma@thomas-stein.com>
2011-10-23 20:49:30 +0200

    "Fork me on github" added

--- content.php
+++ content.php
@@ -14,6 +14,7 @@
 			<?php if (has_post_thumbnail($post->ID)) :?>
 				<?php $thumbnail_post = get_post(get_post_thumbnail_id($post->ID));?>
 				<?php the_post_thumbnail('fullscreen-landscape'); ?>
+				<a href="https://github.com/codelounge/codelounge-exposure-theme"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/5d21241b64dc708fcbb701f68f72f41e9f1fadd6/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
 				<div class="sb_image_desc"><?php echo $thumbnail_post->post_excerpt; ?></div>	
 				<div class="sb_image_credit"><?php echo $thumbnail_post->post_content;?></div>
 				<div class="entenlogo" />
