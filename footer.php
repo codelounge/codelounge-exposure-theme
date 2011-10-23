71345e98c2a804ba35f2264fb60977ee628bb93a
stonefield <info@stonefield.de>
2011-10-23 09:34:12 +0200

    Bugtracker Link added to Footer Navbar

--- footer.php
+++ footer.php
@@ -14,16 +14,17 @@
 
 	<footer id="colophon" role="contentinfo" class="wrapper footer">
 		<div class="footer_wrapper">
-			<div class="alignright">Powered by <a href="http://www.codelounge.de" target="blank">Exposure-Theme</a></div>
+			<div class="alignright">Powered by <a href="http://www.codelounge.de" target="blank">Exposure-Theme</a> (Version: 0.1.1)</div>
 			<div class="alignleft"><span class="entegut">Ente gut, alles gut!</span></div>
 			<div class="clear"></div>
 		</div>
 		
 		<div data-role="footer" class="nav-glyphish">
-			<div data-role="navbar" class="nav-glyphish" data-grid="b">
+			<div data-role="navbar" class="nav-glyphish" data-grid="c">
 				<ul>
 					<li><a href="<?php echo bloginfo('url'); ?>" id="home" data-icon="custom">Home</a></li>
 					<li><a href="#" id="beer" data-icon="custom">Beer</a></li>
+					<li><a href="http://www.codelounge.de/forum/tracker/project-19-wordpress-exposure-theme/" id="radar" data-icon="custom" target="blank">Bugtracker</a></li>
 					<li><a href="#" id="impressum" data-icon="custom">Impressum</a></li>
 				</ul>
 			</div><!-- /navbar -->
