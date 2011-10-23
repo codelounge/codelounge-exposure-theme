f719ed5051561dfcdc4ca4ca0d3aca50e96f3eb2
Thomas Stein <firma@thomas-stein.com>
2011-10-23 21:26:35 +0200

    #000078: Statische Seiten können nicht aufgerufen werden

--- index.php
+++ index.php
@@ -9,23 +9,13 @@
 
 get_header(); ?>
 
-	<?php 
-	// Create a custom query to only show the latest post on the frontpage
-	$query = new WP_Query(array(
-		'post_type' => 'post',
-		'posts_per_page' => 1,
-		'status' => 'publish'
-	));
-	// printr($query);
-	?>
-
 	<div id="primary">
 		<div id="container" role="main">
 				
-		<?php if ( $query->have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
 				
 			<?php /* Start the Loop */ ?>
-				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 				
 					<?php get_template_part( 'content' ); ?>
 
